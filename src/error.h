e7c1bf5ab135f303aaa505247f76f71ed0c6c26f
8i8 <iain.coding@gmail.com>
2021-09-07 23:20:53 +0200

    str variable added to error header

--- src/error.h
+++ src/error.h
@@ -1,3 +1,8 @@
 #include <assert.h>
 #define ERROR(m) (fprintf(stderr, __FILE__ ":%s():%d: error: %s\n", __func__, __LINE__, m))
 #define LOG(m) (fprintf(stderr, __FILE__ ":%s():%d: log: %s\n", __func__, __LINE__, m))
+
+/*
+ * Used for error messages.
+ */
+char str[120];
